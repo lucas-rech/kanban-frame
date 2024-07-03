95532a6b3d4fbba9bed9fc58b2b50a3b790f6f9d
Lucas <lucasrech00@gmail.com>
2024-07-03 19:40:29 -0300

    Initial commit

---
+++ src/main/java/edu/lucasrech/kanban_frame/KanbanFrameApplication.java
@@ -0,0 +1,13 @@
+package edu.lucasrech.kanban_frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanFrameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lucasrech/kanban_frame/KanbanFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lucasrech.kanban_frame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanFrameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
